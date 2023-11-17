3ebc091158e2cd1fc2d89cc63fdb0219d8f332b8
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-17 15:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let a = Number(prompt("Enter First value: "));
+let b = Number(prompt("enter Second value: "));
+let result = (a*2)+b;
+alert("The final result is: "+result);
+ 
